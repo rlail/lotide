777b417edf7da1c887943866af5254412e356f25
Rajan Lail <rajanlail3993@gmail.com>
2023-05-22 13:44:02 -0700

    Week 1

--- eqArrays.js
+++ eqArrays.js
@@ -20,8 +20,8 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false

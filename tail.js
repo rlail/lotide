5871a6bdbb1e7912390528d42a521947de32f354
Rajan Lail <rajanlail3993@gmail.com>
2023-05-22 13:20:10 -0700

    Week 1

---
+++ tail.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function tail(arr) {
+  return arr.slice(1);
+}

8278a33110765167c729cb8673b29e095ec1b3b9
Rajan Lail <rajanlail3993@gmail.com>
2023-05-22 15:37:54 -0700

    Week 1

--- without.js
+++ without.js
@@ -33,5 +33,11 @@ const without = function(source, itemsToRemove) {
   return filteredArray;
 };
 
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 

d3ddcc2f39fa541e88afeba48341233efb6ce194
Rajan Lail <rajanlail3993@gmail.com>
2023-05-22 18:23:30 -0700

    Week 1

---
+++ flatten.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const flatten = function(array) {
+  const flattenedArray = [];
+  for (let element of array) {
+    if (Array.isArray(element)) {
+      for (let item of element) {
+        flattenedArray.push(item);
+      }
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+  return flattenedArray;
+};

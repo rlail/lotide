f98ef9f55cf77284f5de14d9236c0a3aac16e3c9
Rajan Lail <rajanlail3993@gmail.com>
2023-05-24 20:47:25 -0700

    Week 2

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function findKeyByValue(object, value) {
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+// Test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); //  drama
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")); // undefined
+

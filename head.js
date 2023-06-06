b3e58de651572a99a5c852bd49dd928aa44f78e8
Rajan Lail <rajanlail3993@gmail.com>
2023-06-06 16:43:29 -0700

    Remove assertEqual and Test cases. Added module.exports

--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-// Test cases
-assertEqual(head([5, 6, 7]), 5); // Assertion Passed: 5 === 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Assertion Passed: Hello === Hello
+module.exports = head;
 

aa4bb4062bb0b74aad44b4fa29666564bf2fc6f8
Rajan Lail <rajanlail3993@gmail.com>
2023-06-06 16:41:24 -0700

    Add module.exports and remove test cases

--- assertEqual.js
+++ assertEqual.js
@@ -6,6 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Test cases
-assertEqual("Lighthouse Labs", "Bootcamp"); // Assertion Failed: Lighthouse Labs !== Bootcamp
-assertEqual(1, 1); // Assertion Passed: 1 === 1
+module.exports = assertEqual;
+

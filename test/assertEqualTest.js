755ecdac686b90a83fc53a266a24193ac3cbdc96
Rajan Lail <rajanlail3993@gmail.com>
2023-06-06 16:42:40 -0700

    Require assertEqual for the test file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+const assertEqual = require('../assertEqual')
+
+// Test cases
+assertEqual("Lighthouse Labs", "Bootcamp"); // Assertion Failed: Lighthouse Labs !== Bootcamp
+assertEqual(1, 1); // Assertion Passed: 1 === 1

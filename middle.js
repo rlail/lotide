f00c88aaba65cb5c9e9c7aa45cd6cc2aa7fbd804
Rajan Lail <rajanlail3993@gmail.com>
2023-06-07 13:14:16 -0700

    Remove consta assertEqual, eqArrays, assertArraysEqual, and test cases. Add module.exports.

--- middle.js
+++ middle.js
@@ -1,33 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = function(array) {
   const length = array.length;
   if (length <= 2) {
@@ -42,6 +12,5 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([1]), []); 
-assertArraysEqual(middle([1, 2]), []); 
-assertArraysEqual(middle([1, 2, 3]), [2]);
+module.exports = middle; 
+

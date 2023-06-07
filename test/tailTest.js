b90aca445ec404d733474762bf7113d771dd00d2
Rajan Lail <rajanlail3993@gmail.com>
2023-06-07 15:47:37 -0700

    Require 'chai' and '../tail'. Update test cases to use the describe and it functions.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,14 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const tail = require('../tail')
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 2: When the array has one element
-arr = [1];
-result = tail(arr);
-assertEqual(result.length, 0); // Expected output: Assertion Passed
-
-// Test Case 3: When the array is empty
-arr = [];
-result = tail(arr);
-assertEqual(result.length, 0); // Expected output: Assertion Passed
+describe("#tail", () => {
+  it("returns 'tail' for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+  it("returns 'tail' for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("returns 'tail' for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});

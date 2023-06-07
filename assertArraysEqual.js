1392edff999c4447351e902b74421426dc95d0cf
Rajan Lail <rajanlail3993@gmail.com>
2023-06-07 09:23:21 -0700

    Remove assertEqual and eqArrays functions. Require eqArrays. Add module.export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -27,3 +7,7 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
+
+module.exports = assertArraysEqual
+
+

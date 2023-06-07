7d8e1701f66fd9832f3ab371bdac3bd58c816887
Rajan Lail <rajanlail3993@gmail.com>
2023-06-07 15:00:17 -0700

    Require 'chai' and '../head'. Update test cases to use the describe and it functions.

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,19 @@
-const assertEqual = require('../assertEqual')
-const head = require('../head')
+const assert = require('chai').assert;
+const head   = require('../head');
 
 // Test cases
-assertEqual(head([5, 6, 7]), 5); // Assertion Passed: 5 === 5
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Assertion Passed: Hello === Hello
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});

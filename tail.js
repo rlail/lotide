82353e756518139416b8e61c946f0904cc0e1356
Rajan Lail <rajanlail3993@gmail.com>
2023-06-06 17:27:17 -0700

    Remove assertEqual and Test cases. Add module.exports

--- tail.js
+++ tail.js
@@ -1,11 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 function tail(arr) {
   return arr.slice(1);
 }
+
+module.exports = tail;
+
+
